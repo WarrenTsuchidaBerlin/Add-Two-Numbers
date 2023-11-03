e62fb2f59cee6661ccdcacdb9c637b82f88d52c5
WarrenTsuchidaBerlin <warrentberlin@gmail.com>
2023-11-03 19:18:26 -0400

    Update Solution.py
    Fixed typos and added more clarification.

--- Solution.py
+++ Solution.py
@@ -3,14 +3,15 @@
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         #Creates a dummy head node that will make it easier to handle the head node of ListNode (our output list).
+        #Note that this is just a temporary node that simplifies the list construction.
         dummyHead = ListNode(0)
         #Creates a tail pointer, down a few lines it gets updated to always point at the last node in the results list. 
         #Of course, it initially points to the head node as it is the only node at the beginning (i.e. both first and last).
         tail = dummyHead
-        #We need this variable to keep track of any carry-over that results from summing digits.
+        #We need this variable to keep track of any carry-over that results from summing digits that we will do later.
         carry = 0
 
-        #while loop that continues as long as we have more nodes to processn in either l1 or l2 or if there is carry-over.
+        #while loop that continues as long as we have more nodes to process in either l1 or l2 or if there is carry-over.
         while l1 is not None or l2 is not None or carry != 0:
             #The next two lines of code are what extract the current digit from l1 or l2.
             #0 is utilized once end of list has been reached.
@@ -21,13 +22,14 @@ class Solution:
             sum = digit1 + digit2 + carry
             #extracting last digit of sum by taking the modulo 10 of sum.
             digit = sum % 10
-            #Carry-over for the next sum is calculate by performing interger division of the sum by 10.
+            #Carry-over for the next sum is calculated by performing interger division of the sum by 10.
             carry = sum // 10
 
             #Here we build our output/answer. 
-            #A new node is created with the value of digit that was assigned on line 23.
+            #A new node is created with the value of digit that was assigned on line 24.
             newNode = ListNode(digit)
             #Here we are building our output list. We append newNode to the output list.
+            #this line is linking the new node to the end of the result list, making it the new last node in the list.
             tail.next = newNode
             #Update the tail pointer we made earlier to the new node (i.e. the new tail).
             tail = tail.next
@@ -43,6 +45,7 @@ class Solution:
         #The actual answer list starts with the node right after dummyHead, which is what dummyHead.next is pointing to.
         result = dummyHead.next
         #"Good practice" code to prevent memory leak by breaking the link between dummyhead and actual first node.
+        #Code would work fine without this line, and it's more of a precautionary measure.
         dummyHead.next = None
         #provide the answer back to the caller. 
         #Here, we are returning the head of the answer list to whoever called the addTwoNumbers function. 
